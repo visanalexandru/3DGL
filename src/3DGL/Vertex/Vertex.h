19dbb37f26f0650139e43e3fe770cd95d69f3ecc
Visan <alexandruvisan44@yahoo.com>
2019-07-23 18:52:35 +0300

    added textured vertex

--- src/3DGL/Vertex/Vertex.h
+++ src/3DGL/Vertex/Vertex.h
@@ -4,6 +4,7 @@
 
 #ifndef INC_3DGL_VERTEX_H
 #define INC_3DGL_VERTEX_H
+
 #include <glm/glm.hpp>
 #include <glad/glad.h>
 
@@ -11,26 +12,23 @@ struct simple_vertex//basic vertex , only holds position data
 {
     glm::vec3 position;
 
-    simple_vertex(glm::vec3 pos)
-    {
-        position=pos;
+    simple_vertex(glm::vec3 pos) {
+        position = pos;
     }
 
-    simple_vertex()
-    {
-        position=glm::vec3(0,0,0);
+    simple_vertex() {
+        position = glm::vec3(0, 0, 0);
     }
 
 
-    static void enable_attributes()
-    {
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(simple_vertex), (const GLvoid*)offsetof(simple_vertex, position));
+    static void enable_attributes() {
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(simple_vertex),
+                              (const GLvoid *) offsetof(simple_vertex, position));
         glEnableVertexAttribArray(0);
     }
 
 
-    glm::vec3 get_position()
-    {
+    glm::vec3 get_position() {
         return position;
     }
 };
@@ -56,6 +54,40 @@ struct colored_vertex {//vertex that holds position and color data
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(colored_vertex),
                               (const GLvoid *) offsetof(colored_vertex, color));
+        glEnableVertexAttribArray(1);
+
+    }
+
+    glm::vec3 get_position() {
+        return position;
+    }
+
+
+};
+
+
+struct textured_vertex {//vertex that holds position and texture coordinates
+
+    glm::vec3 position;
+    glm::vec2 texture_coords;
+
+    textured_vertex(glm::vec3 pos, glm::vec2 text_coords) {
+        texture_coords = text_coords;
+        position = pos;
+    }
+
+    textured_vertex() {
+        texture_coords = glm::vec2(0, 0);
+        position = glm::vec3(0, 0, 0);
+    }
+
+    static void enable_attributes() {
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(textured_vertex),
+                              (const GLvoid *) offsetof(textured_vertex, position));
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(textured_vertex),
+                              (const GLvoid *) offsetof(textured_vertex, texture_coords));
         glEnableVertexAttribArray(1);
 
     }
