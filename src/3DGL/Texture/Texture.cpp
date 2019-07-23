d10db32cd40f7c2167b487d3b3b5613884e8a07f
Visan <alexandruvisan44@yahoo.com>
2019-07-23 18:28:35 +0300

    added texure class

---
+++ src/3DGL/Texture/Texture.cpp
@@ -0,0 +1,21 @@
+//
+// Created by gvisan on 23.07.2019.
+//
+
+#include "Texture.h"
+
+
+void Texture::delete_texture() const {
+
+    glDeleteTextures(1, &texture_index);
+
+}
+
+Texture::~Texture() {
+    delete_texture();
+}
+
+Texture::Texture() : texture_index(0) {
+
+
+}
---
+++ src/3DGL/Texture/Texture.h
@@ -0,0 +1,30 @@
+//
+// Created by gvisan on 23.07.2019.
+//
+
+#ifndef INC_3DGL_TEXTURE_H
+#define INC_3DGL_TEXTURE_H
+
+#include "stb_image.h"
+#include <string>
+#include <GLFW/glfw3.h>
+
+
+class Texture {
+protected:
+    unsigned texture_index;
+
+    void delete_texture() const;
+
+public:
+    virtual void load_texture(const std::string &path) = 0;
+
+    virtual void bind_texture() const = 0;
+
+    Texture();
+
+    ~Texture();
+};
+
+
+#endif //INC_3DGL_TEXTURE_H
