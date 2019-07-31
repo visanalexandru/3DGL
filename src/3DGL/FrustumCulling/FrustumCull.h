b309d6ac37a2924d52bbb27c183b9b34a8bf1f87
Visan <alexandruvisan44@yahoo.com>
2019-07-31 17:53:54 +0300

    added frustum cull class

---
+++ src/3DGL/FrustumCulling/FrustumCull.h
@@ -0,0 +1,35 @@
+//
+// Created by gvisan on 31.07.2019.
+//
+
+#ifndef INC_3DGL_FRUSTUMCULL_H
+#define INC_3DGL_FRUSTUMCULL_H
+
+
+#include "AABB.h"
+
+#include "../Transformable/Transformable.h"
+
+class FrustumCull {
+private:
+    struct Plane {
+        float distanceToPoint(const glm::vec3 &point) const;
+
+        float distanceToOrigin;
+        glm::vec3 normal;
+
+    };
+
+    Plane m_planes[6];
+
+public:
+    void update(const glm::mat4 &projViewMatrix);
+
+    bool pointInFrustum(glm::vec3 &p);
+
+    bool boxInFrustum(const AABB &box);
+
+};
+
+
+#endif //INC_3DGL_FRUSTUMCULL_H
