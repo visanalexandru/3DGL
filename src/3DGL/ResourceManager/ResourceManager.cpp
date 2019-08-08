8b3db9b7120a9950dfc38937bae6d53ede0f8ab4
Visan <alexandruvisan44@yahoo.com>
2019-08-08 15:02:39 +0300

    added ResourceManager class

---
+++ src/3DGL/ResourceManager/ResourceManager.cpp
@@ -0,0 +1,126 @@
+//
+// Created by gvisan on 08.08.2019.
+//
+
+#include "ResourceManager.h"
+
+
+namespace gl3d {
+
+    void ResourceManager::load_shader(const std::string &name, const std::string &v_path, const std::string &f_path) {
+
+
+        if (contains(resources, name))
+            throw std::runtime_error("shader already exists: " + name);
+
+        FragmentShader f(f_path);
+
+        VertexShader v(v_path);
+
+        ShaderProgram *new_shader = new ShaderProgram(v, f);
+
+        resources[name] = new_shader;
+
+
+    }
+
+
+    void ResourceManager::load_texture(const std::string &name, const std::string &path) {
+
+        if (contains(resources, name))
+            throw std::runtime_error("texture already exists: " + name);
+
+
+        Texture2D *new_texture = new Texture2D();
+        new_texture->load_texture(path);
+
+        resources[name] = new_texture;
+
+    }
+
+
+    void ResourceManager::load_cubemap(const std::string &name, const std::string &path) {
+
+        if (contains(resources, name))
+            throw std::runtime_error("cubemap already exists: " + name);
+
+        Cubemap *new_cubemap = new Cubemap();
+
+        new_cubemap->load_texture(path);
+
+        resources[name] = new_cubemap;
+
+
+    }
+
+
+    const ShaderProgram &ResourceManager::get_shader(const std::string &name) {
+
+        if (contains(resources, name)) {
+
+            Resource *res = resources[name];
+
+            ShaderProgram *derived_cast = dynamic_cast<ShaderProgram *>(res);
+
+            if (derived_cast == nullptr) {
+                throw std::runtime_error("resource is not a shader: " + name);
+
+            }
+
+            return *derived_cast;
+
+        } else throw std::runtime_error("resource does not exist: " + name);
+
+    }
+
+    const Texture2D &ResourceManager::get_texture(const std::string &name) {
+
+        if (contains(resources, name)) {
+
+            Resource *res = resources[name];
+
+            Texture2D *derived_cast = dynamic_cast<Texture2D *>(res);
+
+            if (derived_cast == nullptr) {
+                throw std::runtime_error("resource is not a texture: " + name);
+
+            }
+
+            return *derived_cast;
+
+        } else throw std::runtime_error("resource does not exist: " + name);
+
+    }
+
+    const Cubemap &ResourceManager::get_cubemap(const std::string &name) {
+        if (contains(resources, name)) {
+
+            Resource *res = resources[name];
+
+            Cubemap *derived_cast = dynamic_cast<Cubemap *>(res);
+
+            if (derived_cast == nullptr) {
+                throw std::runtime_error("resource is not a cubemap: " + name);
+
+            }
+
+            return *derived_cast;
+
+        } else throw std::runtime_error("resource does not exist: " + name);
+
+    }
+
+    void ResourceManager::delete_resources() {
+        for (auto &resource:resources) {
+            delete resource.second;
+        }
+
+    }
+
+    ResourceManager::~ResourceManager() {
+        delete_resources();
+
+    }
+
+
+}
---
+++ src/3DGL/ResourceManager/ResourceManager.h
@@ -0,0 +1,56 @@
+//
+// Created by gvisan on 08.08.2019.
+//
+
+#ifndef INC_3DGL_RESOURCEMANAGER_H
+#define INC_3DGL_RESOURCEMANAGER_H
+
+#include "../Shader/ShaderProgram/ShaderProgram.h"
+#include "../Texture/Texture2D/Texture2D.h"
+#include "../Texture/Cubemap/Cubemap.h"
+#include <unordered_map>
+#include <string>
+
+namespace gl3d {
+
+    class ResourceManager {
+    private:
+        std::unordered_map<std::string, Resource *> resources;
+
+        template<class type>
+        bool contains(std::unordered_map<std::string, type> &map, const std::string &to_check);
+
+        void delete_resources();
+
+
+    public:
+        void load_shader(const std::string &name, const std::string &v_path, const std::string &f_path);
+
+        void load_cubemap(const std::string &name, const std::string &path);
+
+        void load_texture(const std::string &name, const std::string &path);
+
+
+        const ShaderProgram &get_shader(const std::string &name);
+
+        const Texture2D &get_texture(const std::string &name);
+
+        const Cubemap &get_cubemap(const std::string &name);
+
+        ~ResourceManager();
+
+
+    };
+
+
+    template<class type>
+    bool ResourceManager::contains(std::unordered_map<std::string, type> &map, const std::string &to_check) {
+
+        return map.find(to_check) != map.end();
+    }
+
+
+}
+
+
+#endif //INC_3DGL_RESOURCEMANAGER_H
